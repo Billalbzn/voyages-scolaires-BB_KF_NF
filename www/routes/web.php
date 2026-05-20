66b3a8ff0bbcf470e7cccc2456bb49db2792b011
FelmitNolan <felmitnolan@gmail.fr>
2026-05-20 15:29:43 +0200

    fix: correction routage web.php et nettoyage base SQLite

--- www/routes/web.php
+++ www/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoyageController;
 use App\Http\Controllers\ParticipantController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,9 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-Route::resource('voyages', VoyageController::class);	
+
+    Route::resource('voyages', VoyageController::class);
+    Route::resource('voyages.participants', ParticipantController::class);
 });
 
 require __DIR__.'/auth.php';
-Route::get('/voyages', function () { return 'Page des voyages'; })->name('voyages.index');
-Route::middleware(['auth'])->group(function () { Route::resource('voyages.participants', ParticipantController::class); });
